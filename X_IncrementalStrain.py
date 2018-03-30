0c8f4176ca2ef08acded630684d9c10c65ef9c14
Russle_Laptop <mfscales@gmail.com>
2018-03-30 12:21:16 -0500

    Alternative inc strain methods I came up with are gone.  Only dFF is used now.  Calcs mean of whole box, mean of passing, passing point to point, and max point.  Plotter permits plotting of mean or max.

--- X_IncrementalStrain.py
+++ X_IncrementalStrain.py
@@ -8,10 +8,17 @@ import matplotlib.pyplot as p
 from mysqrtm import mysqrtm
 import os
 from sys import argv
+from scipy.io import savemat
 
 expt = argv[1]
-proj = 'TTGM-{}_FS19SS6'.format(expt)
+proj = 'TT2-{}_FS19SS6'.format(expt)
+print('\n')
 print(proj)
+
+if expt == '18':
+    alpha = n.nan
+else:
+    alpha = 2.0
 
 expt = int( proj.split('_')[0].split('-')[1])
 FS = int( proj.split('_')[1].split('SS')[0].split('S')[1] )
@@ -20,42 +27,59 @@ arampath = '../{}/AramisBinary'.format(proj)
 prefix = '{}_'.format(proj)
 savepath = '../{}'.format(proj)
 
-key = n.genfromtxt('{}/../ExptSummary.dat'.format(savepath), delimiter=',')
-(extype, tube, alpha,
- alpha_true, Rm, thickness) = key[ key[:,0] == expt, 1:7].ravel()
-
 os.chdir(savepath)
 
 # [0]Stg [1]Time [2]AxSts [3]ShSts [4]AxForce [5]Torque [6]MTS Disp [7]MTS Rot
 STF = n.genfromtxt('STF.dat', delimiter=',')
 last = int(STF[-1,0])
+maxi, maxj = n.genfromtxt('Max10.dat', delimiter=',')[0, 1:3]
 Xmin,Xmax,Ymin,Ymax = n.genfromtxt('box_limits.dat', delimiter=',')
 profStg = n.genfromtxt('prof_stages.dat', delimiter=',', dtype=int)
 LL = profStg[2]
 
-# Type:  0=Radial, 1=Sigma-Tau Corner, 2=Tau-Sigma Corner
-if extype == 0:
-    # Radial:  Start filtering immediately
-    filter_start = 0
-elif extype == 1:
-    # Sig-->Tau :  Use rotation to set fiilter start
-    filter_start = n.nonzero(STF[:,-1] >= STF[:,-1].max()*.1 )[0][0]
-elif extype == 2:
-    # Tau-->Sig  Use displacement
-    filter_start = n.nonzero(STF[:,-2] >= STF[:,-2].max()*.1 )[0][0]
+def increm_strains(A00,A01,A10,A11,B00,B01,B10,B11):
+    de00 = (2*((A00 - B00)*(A11 + B11) - (A01 - B01)*(A10 + B10))/
+            ((A00 + B00)*(A11 + B11) - (A01 + B01)*(A10 + B10))
+           )
+    de01 = ((-(A00 - B00)*(A01 + B01) + (A00 + B00)*(A01 - B01) + 
+            (A10 - B10)*(A11 + B11) - (A10 + B10)*(A11 - B11))/
+            ((A00 + B00)*(A11 + B11) - (A01 + B01)*(A10 + B10))
+           )
+    de11 = (2*((A00 + B00)*(A11 - B11) - (A01 + B01)*(A10 - B10))/
+            ((A00 + B00)*(A11 + B11) - (A01 + B01)*(A10 + B10))
+           )
+    if type(de00) is n.ndarray:
+        return de00.mean(), de01.mean(), de11.mean()
+    else:
+        return de00, de01, de11
 
-# Kelin's method.  Col 1 = vm, col2 = h8
+def deeq(de00, de01, de11, sig00, sig01, sig11, sigeq):
+    return (sig00*de00 + sig11*de11 - 2*sig01*de01)/sigeq
+    
+# Average F for all points in box
 deeq1 = n.empty((last+1,2))
-# Conventional strain def method.  Col 1 = vm, col2 = h8
+# Averaging F  for passing points
 deeq2 = n.empty((last+1,2))
-# I think I had a bug in deeq2
+# Point to point for passing points
 deeq3 = n.empty((last+1,2))
-# Calculating EigU, and rotating back
+# Max point (no choice but point to point)
 deeq4 = n.empty((last+1,2))
-# Averaging F then using same F next stage
-deeq5 = n.empty((last+1,2))
+# A dictionary for passing points to be saved
+data = {}
 for k in range(1,last+1):
     if k%25 == 0:  print(k, end=',', flush=True)
+
+    sig00 = STF[k,2]/2  # Hoop sts (assumed 1/2 axial)
+    sig11 = STF[k,2]   # Ax sts
+    sig01 = STF[k,3]   # Sh sts
+    # Mises equivalent sts
+    sigvm = n.sqrt(sig11**2 + sig00**2 - sig00*sig11 + 3*sig01**2)
+    # Principle stresses
+    s1 = sig00/2 + sig11/2 + sqrt(sig00**2 - 2*sig00*sig11 + 4*sig01**2 + sig11**2)/2
+    s2 = sig00/2 + sig11/2 - sqrt(sig00**2 - 2*sig00*sig11 + 4*sig01**2 + sig11**2)/2
+    # Hosford eq. sts
+    sigh8 = (((s1-s2)**8+(s2-0)**8+(0-s1)**8)/2)**(1/8)
+
     # Stage k
     A = n.load('{}/{}{:.0f}.npy'.format(arampath,prefix,k))
     # Stage k-1
@@ -66,6 +90,24 @@ for k in range(1,last+1):
     Alex, Aley, Alexy = ( [] for _ in range(3) )    
     # Downsize A
     Abox = A[ (A[:,2]>=Xmin) & (A[:,2]<=Xmax) & (A[:,3]>=Ymin) & (A[:,3]<=Ymax), :]
+    Bbox = B[ (B[:,2]>=Xmin) & (B[:,2]<=Xmax) & (B[:,3]>=Ymin) & (B[:,3]<=Ymax), :]
+
+    ## deeq1:  Average all points in the box
+    de00, de01, de11 = increm_strains(Abox[:,8].mean(), Abox[:,9].mean(), Abox[:,10].mean(), Abox[:,11].mean(),
+                                   Bbox[:,8].mean(), Bbox[:,9].mean(), Bbox[:,10].mean(), Bbox[:,11].mean())
+    deeq1[k,0] = deeq(de00, de01, de11, sig00, sig01, sig11, sigvm)
+    deeq1[k,1] = deeq(de00, de01, de11, sig00, sig01, sig11, sigh8)
+
+    ## deeq4:  Max Point
+    maxptA = A[ (A[:,0] == maxi) & (A[:,1] == maxj) ].ravel()
+    maxptB = B[ (B[:,0] == maxi) & (B[:,1] == maxj) ].ravel()
+    if (len(maxptA) == 0) or (len(maxptB) == 0):
+        deeq4[k] = 0
+    else:
+        de00, de01, de11 = increm_strains(*(*maxptA[8:], *maxptB[8:]))
+        deeq4[k,0] = deeq(de00, de01, de11, sig00, sig01, sig11, sigvm)
+        deeq4[k,1] = deeq(de00, de01, de11, sig00, sig01, sig11, sigh8)
+    
     # Break it down
     F=Abox[:,-4:].reshape(len(Abox[:,0]),2,2)   # A "stack" of 2x2 deformation gradients 
     FtF = n.einsum('...ji,...jk',F,F)
@@ -103,7 +145,7 @@ for k in range(1,last+1):
             A11.append( Abox[rng,11][locLEp])
             Alex.append( LEx[rng][locLEp] )
             Aley.append( LEy[rng][locLEp] )            
-            Alexy.append( LExy[rng][locLEp] )            
+            Alexy.append( LExy[rng][locLEp] )         
     # Convert these lists to arrays so I can boolean index
     for varname in 'LEp,NEy,NEx,gamma,aramX,aramY,A00,A01,A10,A11,Alex,Aley,Alexy'.split(','):
         exec('{0} = array({0})'.format(varname))
@@ -113,15 +155,13 @@ for k in range(1,last+1):
         ratio = NEx / NEy
     ratioAvg = nanmean(ratio)
     ratioSDEV = nanstd(ratio)    
-    if k >= filter_start:
-        # Then filter
-        passed = (ratio >= ratioAvg - 0.5 * ratioSDEV) & (ratio <= ratioAvg + 0.5 * ratioSDEV)
-    else:
-        # Otherwise, don't bother filtering
-        passed = n.ones_like(ratio, dtype=bool)
+    passed = (ratio >= ratioAvg - 0.5 * ratioSDEV) & (ratio <= ratioAvg + 0.5 * ratioSDEV)
     # Boolean index all the arrays I'm carrying
     for varname in 'LEp,NEy,gamma,aramX,aramY,A00,A01,A10,A11,Alex,Aley,Alexy'.split(','):
         exec('{0} = {0}[passed]'.format(varname))
+    
+    data['stage_{}'.format(k)] = n.c_[aramX,aramY,A00,A01,A10,A11]
+    
     locmax = n.argmax( LEp )
     # So now I have LEp, NEy, ..., A11 for all the passing points.  And the loc of max
     # I need to get the same aramX and Y points from stage k-1
@@ -148,60 +188,9 @@ for k in range(1,last+1):
     # B is F_i-1, A is F
     # dF = F_i - F_i-1 = A - B
     # F.inv() = inv( (F_i + F_i-1)/2 ) = inv((A+B)/2)
-    de00 = (2*((A00 - B00)*(A11 + B11) - (A01 - B01)*(A10 + B10))/
-            ((A00 + B00)*(A11 + B11) - (A01 + B01)*(A10 + B10))
-           )
-    de01 = ((-(A00 - B00)*(A01 + B01) + (A00 + B00)*(A01 - B01) + 
-            (A10 - B10)*(A11 + B11) - (A10 + B10)*(A11 - B11))/
-            ((A00 + B00)*(A11 + B11) - (A01 + B01)*(A10 + B10))
-           )
-    de11 = (2*((A00 + B00)*(A11 - B11) - (A01 + B01)*(A10 - B10))/
-            ((A00 + B00)*(A11 + B11) - (A01 + B01)*(A10 + B10))
-           )
-    sig00 = STF[k,2]/2  # Hoop sts (assumed 1/2 axial)
-    sig11 = STF[k,2]   # Ax sts
-    sig01 = STF[k,3]   # Sh sts
-    # Mises equivalent sts
-    sigvm = n.sqrt(sig11**2 + sig00**2 - sig00*sig11 + 3*sig01**2)
-    # Principle stresses
-    s1 = sig00/2 + sig11/2 + sqrt(sig00**2 - 2*sig00*sig11 + 4*sig01**2 + sig11**2)/2
-    s2 = sig00/2 + sig11/2 - sqrt(sig00**2 - 2*sig00*sig11 + 4*sig01**2 + sig11**2)/2
-    # Hosford eq. sts
-    sigh8 = (((s1-s2)**8+(s2-0)**8+(0-s1)**8)/2)**(1/8)
-
-    deeq1[k,0] = (sig00*de00.mean() + sig11*de11.mean() - 2*sig01*de01.mean())/sigvm
-    deeq1[k,1] = (sig00*de00.mean() + sig11*de11.mean() - 2*sig01*de01.mean())/sigh8
-    
-    de00 = n.log(1+A00) - n.log(1+B00)
-    de01 = (n.arctan2(A01,A11) - n.arctan2(B01,B11))/2
-    de11 = n.log(1+A11) - n.log(1+B11)
-    deeq2[k,0] = (sig00*de00.mean() + sig11*de11.mean() - 2*sig01*de01.mean())/sigvm    
-    deeq2[k,1] = (sig00*de00.mean() + sig11*de11.mean() - 2*sig01*de01.mean())/sigh8
-
-    de00 = n.log(A00) - n.log(B00)
-    de01 = (n.arctan2(A01,A11) - n.arctan2(B01,B11))/2
-    de11 = n.log(A11) - n.log(B11)
-    deeq3[k,0] = (sig00*de00.mean() + sig11*de11.mean() - 2*sig01*de01.mean())/sigvm    
-    deeq3[k,1] = (sig00*de00.mean() + sig11*de11.mean() - 2*sig01*de01.mean())/sigh8    
-
-    # Form U for B's Fs so I can get Blex, Bley, Blexy
-    F = n.empty((len(B00),2,2))
-    F[:,0,0], F[:,0,1], F[:,1,0], F[:,1,1] = B00, B01, B10, B11
-    FtF = n.einsum('...ji,...jk',F,F)
-    U = mysqrtm( FtF )     #Explicit calculation of sqrtm    
-    eigU, V = n.linalg.eigh(U)
-    LEprin = n.log(eigU)
-    # Now rotate the principle log strains back to x, y using the eigenvectors
-    # It turns out that LEx, and LEy are very close to just taking n.log(U)
-    # And the resulting shears are very close to the off-diagonals of U
-    LExy = n.einsum('...ij,...j,...jk',V,LEprin,V)
-    Blex, Bley = LExy[:,0,0], LExy[:,1,1]    
-    Blexy = LExy[:,0,1]    
-    de00 = (Alex-Blex)
-    de01 = (Alexy-Blexy)
-    de11 = (Aley-Bley)
-    deeq4[k,0] = (sig00*de00.mean() + sig11*de11.mean() - 2*sig01*de01.mean())/sigvm    
-    deeq4[k,1] = (sig00*de00.mean() + sig11*de11.mean() - 2*sig01*de01.mean())/sigh8    
+    de00, de01, de11 = increm_strains(A00,A01,A10,A11,B00,B01,B10,B11)
+    deeq3[k,0] = deeq(de00, de01, de11, sig00, sig01, sig11, sigvm)
+    deeq3[k,1] = deeq(de00, de01, de11, sig00, sig01, sig11, sigh8)
 
     ## Last method:  Average F!
     if k == 1:
@@ -209,36 +198,29 @@ for k in range(1,last+1):
         B01t, B10t = 0, 0
 
     A00, A01, A10, A11 = [i.mean() for i in (A00, A01, A10, A11)]
-    
-    de00 = (2*((A00 - B00t)*(A11 + B11t) - (A01 - B01t)*(A10 + B10t))/
-            ((A00 + B00t)*(A11 + B11t) - (A01 + B01t)*(A10 + B10t))
-           )
-    de01 = ((-(A00 - B00t)*(A01 + B01t) + (A00 + B00t)*(A01 - B01t) + 
-            (A10 - B10t)*(A11 + B11t) - (A10 + B10t)*(A11 - B11t))/
-            ((A00 + B00t)*(A11 + B11t) - (A01 + B01t)*(A10 + B10t))
-           )
-    de11 = (2*((A00 + B00t)*(A11 - B11t) - (A01 + B01t)*(A10 - B10t))/
-            ((A00 + B00t)*(A11 + B11t) - (A01 + B01t)*(A10 + B10t))
-           )
+
+    de00, de01, de11 = increm_strains(A00,A01,A10,A11,B00t,B01t,B10t,B11t)
 
     B00t, B01t, B10t, B11t = [i for i in (A00, A01, A10, A11)]           
            
-    deeq5[k,0] = (sig00*de00 + sig11*de11 - 2*sig01*de01)/sigvm    
-    deeq5[k,1] = (sig00*de00 + sig11*de11 - 2*sig01*de01)/sigh8        
+    deeq2[k,0] = (sig00*de00 + sig11*de11 - 2*sig01*de01)/sigvm    
+    deeq2[k,1] = (sig00*de00 + sig11*de11 - 2*sig01*de01)/sigh8        
+
+stage_1 = data['stage_1'].copy()
+stage_1[:,2:] = 1, 0, 0, 1    
+data['stage_0'] = stage_1
         
-    
 deeq1[0] = 0
 deeq2[0] = 0
 deeq3[0] = 0
 deeq4[0] = 0
-deeq5[0] = 0
+savemat('{}_PassingPts'.format(proj), data)
 
 for i in [deeq1, deeq2, deeq3, deeq4]:
     i[ n.any(n.isnan(i), axis=1) ] = 0
 
-headerline = ('[0]Stage, [1]Kelin VM, [2]Kelin H8, [3]Alternate VM, [4]Alternate H8\n' + 
-              '[5]BugFix AltVM, [6]BugFix AltH8, [7]VLEV VM, [8]VLEV H8, [9]AverageF vm, [10]AverageF h8')
-X = n.c_[STF[:,0], deeq1.cumsum(axis=0), deeq2.cumsum(axis=0), deeq3.cumsum(axis=0), deeq4.cumsum(axis=0), deeq5.cumsum(axis=0)]
+headerline = ('[0]Stage, [1]AvgF-All in Box-VM, [2]H8, [3]AvgF-Passing-VM, [4]H8, [5]Passing P2P-VM, [6]H8, [7]MaxPt-VM, [8]H8')
+X = n.c_[STF[:,0], deeq1.cumsum(axis=0), deeq2.cumsum(axis=0), deeq3.cumsum(axis=0), deeq4.cumsum(axis=0)]
 n.savetxt('IncrementalStrain.dat', X=X, header=headerline,
-        fmt = '%.0f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f'
+        fmt = '%.0f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f'
         )
--- X_IncrementalStrainPlot.py
+++ X_IncrementalStrainPlot.py
@@ -9,30 +9,30 @@ import os
 '''
 Plots published values and ONLY Kelin's incremental values, pt-to-pt and averaged
 '''
-
-expts = n.array([1, 2, 3, 4, 5, 7, 9, 10])#, 12, 13, 14, 15])
+max_or_mean = 'max'
+expts = n.array([ 31,   35, 20, 22,  24, 17, 27,   34,  32,   9,  30,  16, 15, 18])#, 12, 13, 14, 15])
+alpha = n.array([.25, .375, .5, .5, .75,  1,  1, 1.25, 1.5, 2.0, 3.0, 3.5, 4.0, n.nan])
 #expts = n.array([2,12,13,3,14,15])
 # [0]Expt No., [1]Expt Type, [2]Tube No., [3]Alpha, [4]Alpha-True, 
 # [5]Mean Radius, [6]Thickness, [7]Eccentricity
 # Type:  0=Radial, 1=Sigma-Tau Corner, 2=Tau-Sigma Corner
-key = n.genfromtxt('../ExptSummary.dat', delimiter=',')
 
-for k, x in enumerate(expts):
+for k, (x,alp) in enumerate(zip(expts,alpha)):
 
     # [0]Stage [1]Time [2]AxSts [3]ShSts [4]Delta/L [5]Phi. Lg = 0.657804 inch
-    rot = n.genfromtxt('../TTGM-{}_FS19SS6/disp-rot.dat'.format(x), delimiter=',', usecols=(5))
-    # [0]Stage, [1]Kelin VM, [2]Kelin H8, [3]Alternate VM, [4]Alternate H8
-    e = n.genfromtxt('../TTGM-{}_FS19SS6/IncrementalStrain.dat'.format(x), delimiter=',')
+    rot = n.genfromtxt('../TT2-{}_FS19SS6/disp-rot.dat'.format(x), delimiter=',', usecols=(5))
+    # [0]Stage, [1]AvgF-All in Box-VM, [2]H8, [3]AvgF-Passing-VM, [4]H8, [5]Passing P2P-VM, [6]H8, [7]MaxPt-VM, [8]H8
+    e = n.genfromtxt('../TT2-{}_FS19SS6/IncrementalStrain.dat'.format(x), delimiter=',')
     # # [0]Stage [1]Time [2]NumPtsPassed [3]AxSts [4]ShSts [5]NEx 
     # [6]NEy [7]Gamma [8]F11-1 [9]F22-1 [10]atan(F12/F22) [11]epeq
-    sig, tau, dmean = n.genfromtxt('../TTGM-{}_FS19SS6/mean.dat'.format(x), 
+    sig, tau, dmean = n.genfromtxt('../TT2-{}_FS19SS6/mean.dat'.format(x), 
             delimiter=',', usecols=(3,4,11), unpack=True)
+    dmax = n.genfromtxt('../TT2-{}_FS19SS6/MaxPt.dat'.format(x))[:,-1]
     
-    expinfo = key[ key[:,0] == x ].ravel()
-    if n.isnan(expinfo[3]):
-        masterlabel = '{:.0f}; $\\infty$ ; {:.0f}'.format(x,expinfo[2])
+    if n.isnan(alp):
+        masterlabel = '{:.0f}; $\\infty$ '.format(x)
     else:
-        masterlabel = '{:.0f}; {}; {:.0f}'.format(x,expinfo[3],expinfo[2])
+        masterlabel = '{:.0f}; {}'.format(x,alp)
     
     triax = ((sig/2)/n.sqrt(.75*(sig**2+4*tau**2)))[-1]
 
@@ -40,16 +40,31 @@ for k, x in enumerate(expts):
     if k == 0:
         fig0 = p.figure()
         ax0 = fig0.add_subplot(111)
-    master, = ax0.plot(rot, dmean, label=masterlabel)
-    line2, = ax0.plot(rot, e[:,1],'--', color=master.get_color())
-    line3, = ax0.plot(rot, e[:,-2], ':', color=master.get_color())
+    
+    if max_or_mean == 'max':
+        data = dmax
+        col = 7
+    elif max_or_mean == 'mean':
+        data = dmean
+        col = 5
+
+    master, = ax0.plot(rot, data, label=masterlabel)
+    line2, = ax0.plot(rot, e[:,col],'--', color=master.get_color())
+    line3 = 'spare'
+    if max_or_mean == 'mean':
+        line3, = ax0.plot(rot, e[:,3], ':', color=master.get_color())
+
     if x == expts[-1]:
         ax0.axis(xmin=0, ymin=0)
-        ax0.set_title('Mean Strain vs Rotation')
+        ax0.set_title('M{} Strain vs Rotation'.format(max_or_mean[1:]))
         ax0.set_xlabel('$\\phi^\\circ$')
         ax0.set_ylabel('$\\mathsf{e}^{\\mathsf{p}}_{\\mathsf{e}}$')
         lines = [master, line2, line3]
-        leg02 = ax0.legend(lines, ['Normal', 'Increm/P2P', 'Increm/Avg'], loc='lower right')
+        labels = ['Normal', 'Increm/P2P', 'Increm/Avg']
+        if max_or_mean == 'max':
+            lines = lines[:2]
+            labels = labels[:2]
+        leg02 = ax0.legend(lines, labels, loc='lower right')
         p.setp(leg02.get_lines(), color='k')
         f.ezlegend(ax0)
         ax0.add_artist(leg02)
@@ -60,17 +75,19 @@ for k, x in enumerate(expts):
         fig1 = p.figure()
         ax1 = fig1.add_subplot(111)
     lines = []
-    lines.append( ax1.plot(triax, dmean[-1], 'o', color='C0', mfc='none', label='Traditional')[0] )
-    lines.append(ax1.plot(triax, e[-1,1], 's', color='C1', label='Increm/P2P')[0])
-    lines.append(ax1.plot(triax, e[-1,-2], '^', color='C2', label='Increm/Avg')[0])
-    if n.isnan(expinfo[3]):
-        tx = ax1.text(triax,1.7,'\n$\\infty$',
+    lines.append( ax1.plot(triax, data[-1], 'o', color='C0', mfc='none', label='Traditional')[0] )
+    lines.append(ax1.plot(triax, e[-1,col], 's', color='C1', label='Increm/P2P')[0])
+    if max_or_mean == 'mean':
+        lines.append(ax1.plot(triax, e[-1,3], '^', color='C2', label='Increm/Avg')[0])
+    
+    if n.isnan(alp):
+        tx = ax1.text(triax,1.8,'\n$\\infty$',
                 color=master.get_color(),ha='center',va='top',size=10)
     else:
-        tx = ax1.text(triax,1.7,'\n{}'.format(expinfo[3]),
+        tx = ax1.text(triax,1.8,'{}'.format(alp),
                 color=master.get_color(),ha='center',va='top',size=10)
     if x == expts[-1]:
-        ax1.axis([0,0.6,0,1.7])
+        ax1.axis([0,0.6,0,1.8])
         ax1.set_xlabel('$\\sigma_{\\mathsf{m}}/\\sigma_{\\mathsf{e}}$')
         ax1.set_ylabel('$\\mathsf{e}^{\\mathsf{p}}_{\\mathsf{e}}$')
         leg1 = ax1.legend(lines, [i.get_label() for i in lines],
@@ -78,8 +95,9 @@ for k, x in enumerate(expts):
                 bbox_transform=ax1.transAxes,
                 handlelength=0)
         [i.set_color(leg1.get_lines()[k].get_mec()) for k,i in enumerate(leg1.get_texts())]
+        f.eztext(ax1, 'M{} values'.format(max_or_mean[1:]), 'bl')
         f.myax(ax1)
 
-fig0.savefig('../8 - New Incremental eeq-rot.png', bbox_inches='tight', dpi=175)
-fig1.savefig('../9 - New Incremental eeq-triax.png', bbox_inches='tight', dpi=175)
+fig0.savefig('../8 - Incremental {} eeq-rot.png'.format(max_or_mean), bbox_inches='tight', dpi=175)
+fig1.savefig('../9 - Incremental {} eeq-triax.png'.format(max_or_mean), bbox_inches='tight', dpi=175)
 p.show('all')
